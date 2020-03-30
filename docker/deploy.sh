cd8ce9a21b0a61d55ff2c2a6fee144d3ed96377e
Valentin Kettner <vakevk+git@gmail.com>
2020-03-30 12:18:49 +0200

    Update deploy.sh

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -4,6 +4,17 @@ set -euo pipefail
 
 image_name=$1
 sudo apt-get update && sudo apt-get install -y python-pip && sudo pip install awscli
-$(aws ecr get-login --no-include-email --region $AWS_REGION)
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin;
 docker build --tag $REGISTRY_URI:$image_name -f docker/Dockerfile .
 docker push $REGISTRY_URI:$image_name
+
+if [ "$tag_name" == "master" ] && [ -n "$AUTODEPLOY_URL" ] && [ -n "$AUTODEPLOY_TOKEN" ]; then
+    # Notifying webhook
+    curl -s  \
+      --output /dev/null \
+      --write-out "%{http_code}" \
+      -H "Content-Type: application/json" \
+      -X POST \
+      -d '{"push_data": {"tag": "'$AUTODEPLOY_TAG'" }}' \
+      $AUTODEPLOY_URL
+fi
