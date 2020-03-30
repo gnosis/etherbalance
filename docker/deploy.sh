2b67b37ad8b1220b81ae127c2723236c80cc9ff8
Valentin Kettner <vakevk+git@gmail.com>
2020-03-30 13:46:14 +0200

    Make deploy script executable

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-set -xeuo pipefail
+set -veuo pipefail
 
-echo deploy_0
 tag_name=$1
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin;
-echo deploy_1
 docker build --tag $REGISTRY_URI:$tag_name -f docker/Dockerfile .
-echo deploy_2
 docker push $REGISTRY_URI:$tag_name
-echo deploy_3
 
 if [ "$tag_name" == "master" ] && [ -n "$AUTODEPLOY_URL" ]; then
     # Notifying webhook
@@ -21,4 +17,3 @@ if [ "$tag_name" == "master" ] && [ -n "$AUTODEPLOY_URL" ]; then
       -d '{"push_data": {"tag": "'$tag_name'" }}' \
       $AUTODEPLOY_URL
 fi
-echo deploy_4
