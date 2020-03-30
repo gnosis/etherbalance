c45a1c0818174745906ce3fdef6518a6a9cfbe59
Valentin Kettner <vakevk+git@gmail.com>
2020-03-30 12:43:18 +0200

    Update deploy.sh

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -7,13 +7,13 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin;
 docker build --tag $REGISTRY_URI:$tag_name -f docker/Dockerfile .
 docker push $REGISTRY_URI:$tag_name
 
-if [ "$tag_name" == "master" ] && [ -n "$AUTODEPLOY_URL" ] && [ -n "$AUTODEPLOY_TOKEN" ]; then
+if [ "$tag_name" == "master" ] && [ -n "$AUTODEPLOY_URL" ]; then
     # Notifying webhook
     curl -s  \
       --output /dev/null \
       --write-out "%{http_code}" \
       -H "Content-Type: application/json" \
       -X POST \
-      -d '{"push_data": {"tag": "'$AUTODEPLOY_TAG'" }}' \
+      -d '{"push_data": {"tag": "'$tag_name'" }}' \
       $AUTODEPLOY_URL
 fi
