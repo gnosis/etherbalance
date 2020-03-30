fbf67db44c8010dbea4d512ca3746c626bc7ac69
Valentin Kettner <vakevk+git@gmail.com>
2020-03-30 13:34:09 +0200

    Update deploy.sh

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-set -veuo pipefail
+set -xeuo pipefail
 
+echo deploy_0
 tag_name=$1
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin;
+echo deploy_1
 docker build --tag $REGISTRY_URI:$tag_name -f docker/Dockerfile .
+echo deploy_2
 docker push $REGISTRY_URI:$tag_name
+echo deploy_3
 
 if [ "$tag_name" == "master" ] && [ -n "$AUTODEPLOY_URL" ]; then
     # Notifying webhook
@@ -17,3 +21,4 @@ if [ "$tag_name" == "master" ] && [ -n "$AUTODEPLOY_URL" ]; then
       -d '{"push_data": {"tag": "'$tag_name'" }}' \
       $AUTODEPLOY_URL
 fi
+echo deploy_4
