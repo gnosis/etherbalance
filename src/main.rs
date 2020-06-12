fb0a3e629023355d8042713527721af613e62fb3
Ben Smith <bh2smith@gmail.com>
2020-06-12 10:44:50 +0200

    printed opt one line too early!

--- src/main.rs
+++ src/main.rs
@@ -77,8 +77,8 @@ pub fn u256_to_f64(value: U256) -> f64 {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Beginning service with configuration parameters {:?}", opt);
     let opt = Opt::from_args();
+    println!("Beginning service with configuration parameters {:?}", opt);
     let config: config::Config = toml::from_str(&std::fs::read_to_string(opt.config)?)?;
     println!("Monitoring accounts {:?}", config);
 
