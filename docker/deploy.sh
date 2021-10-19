36619806df87ba535f5c2583fdaeda6778268b2a
Ben Smith <bh2smith@gmail.com>
2021-10-19 15:01:59 +0200

    remove autodeploy too much effort for very few changes

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -6,14 +6,3 @@ tag_name=$1
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin;
 docker build --tag $REGISTRY_URI:$tag_name -f docker/Dockerfile .
 docker push $REGISTRY_URI:$tag_name
-
-if [ -n "$AUTODEPLOY_URL" ]; then
-    # Notifying webhook
-    curl -s  \
-      --output /dev/null \
-      --write-out "%{http_code}" \
-      -H "Content-Type: application/json" \
-      -X POST \
-      -d '{"push_data": {"tag": "'$tag_name'" }}' \
-      $AUTODEPLOY_URL
-fi
